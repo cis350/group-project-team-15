36e7ef711823b409a8bd7cb97f8ab85a000de960
pattokseas <mpattok@seas.upenn.edu>
2024-03-29 19:38:20 -0400

    try to use heroku port

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@
 // import the express app
 const webapp = require('./server');
 
-const port = 8080;
+const port = process.env.PORT || 8080;
 // start the web server
 webapp.listen(port, () => {
   console.log('Server running on port', port);
